1bccd495478a3386965c28fe0bab8833dc4d12e0
Hiroki Kitagawa <m210426@hiroshima-u.ac.jp>
2021-07-07 01:16:24 +0000

    task2 complete

--- rpn.go
+++ rpn.go
@@ -1,9 +1,11 @@
 package main
 
 func Calculate(x int, y int, e string) int {
-	//	var str_arr = []string{}
-
-	return 42
+	var ans int
+	if e == "+" {
+		ans = x + y
+	}
+	return ans
 }
 
 func main() {
--- rpn_test.go
+++ rpn_test.go
@@ -2,10 +2,17 @@ package main
 
 import "testing"
 
-func TestCalculate01(t *testing.T) {
-	num := Calculate(1,1,"+")
-	if num != 42 {
-		t.Error("Test01 is failed")
+//func TestCalculate01(t *testing.T) {
+	//num := Calculate(1,1,"+")
+	//if num != 42 {
+		//t.Error("Test01 is failed")
+	//}
+//}
+
+func TestCalculate02(t *testing.T) {
+	num := Calculate(2,3,"+")
+	if num != 5 {
+		t.Error("Test02 is failed")
 	}
 }
 
